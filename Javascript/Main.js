9615b4b5c92c79c52ed35d2206bf278f45fa88d4
David Campbell <legacyofheru@Davids-iMac.local>
2022-09-23 12:21:07 -0400

    first commit

---
+++ Javascript/Main.js
@@ -0,0 +1,28 @@
+console.log("Hello Dave");
+console.error("this is an error");
+console.warn("This is a warning");
+let score= 100;
+
+console.log(score);
+
+// String, Numbers, Boolean, null, undefined
+
+const name = "Cleshay";
+const age = 41;
+const rating = 6.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+
+// 3 Methods of using the console
+
+//Concatenation
+console.log('My name  is ' + name + ' and I am ' + age);
+// Template String
+console.log(`My name is ${name} and I am still ${age}`)
+//Console log Template String
+const hello = `My name is ${name} and I am still ${age}`
+
+console.log(hello)
